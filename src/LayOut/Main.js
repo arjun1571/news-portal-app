f9dae8975417cdc3dcecc8f069df5b93df6c025b
Arjun <arjundasback515@gmail.com>
2023-06-22 23:28:34 +0600

    project layout and react bootsrap install done

--- src/LayOut/Main.js
+++ src/LayOut/Main.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Sheared/Header/Header';
+import Fotter from '../Pages/Sheared/Fotter/Fotter';
+import LeftSideNav from '../Pages/Sheared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../Pages/Sheared/RightSideNav/RightSideNav';
 
 const Main = () => {
     return (
         <div>
-            <h1>this is main</h1>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="2">
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg="7">
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg="3">
+                        <RightSideNav></RightSideNav>
+                    </Col>
+                </Row>
+            </Container>
+            <Fotter></Fotter>
         </div>
     );
 };
---
+++ src/Pages/Sheared/Fotter/Fotter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fotter = () => {
+    return (
+        <div>
+            <p><small>copyright @2023</small></p>
+        </div>
+    );
+};
+
+export default Fotter;
