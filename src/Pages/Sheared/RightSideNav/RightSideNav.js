52e53763456ba0ea9df68e97b74e897ff8ef955a
Arjun <arjundasback515@gmail.com>
2023-06-23 16:11:18 +0600

    right side area button done

--- src/Pages/Sheared/RightSideNav/RightSideNav.js
+++ src/Pages/Sheared/RightSideNav/RightSideNav.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaFacebook, FaGithub , FaWhatsapp, FaTwitter,FaTwitch } from 'react-icons/fa';
 const RightSideNav = () => {
     return (
         <div>
-            <h2>this is right side</h2>
+            <ButtonGroup  vertical>
+                <Button className='mb-2' variant="outline-primary">Login with Google</Button>
+                <Button variant="outline-dark">Login with GitHub</Button>
+            </ButtonGroup>
+            <h4 className='mb-3 mt-3'>Find On Us</h4>
+            <ListGroup>
+                <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                <ListGroup.Item className='mb-2'><FaGithub></FaGithub> GitHub</ListGroup.Item>
+                <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp> Whatsapp</ListGroup.Item>
+                <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                <ListGroup.Item className='mb-2'><FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+            </ListGroup>
         </div>
     );
 };
